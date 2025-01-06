435833b227d1d7999dde98d87c7af9b41b365b7d
Vitalii Shapovalov <martyit@ukr.net>
2025-01-06 12:05:15 +0100

    server listen

--- app.mjs
+++ app.mjs
@@ -61,6 +61,6 @@ wss.on('connection', (ws) => {
   })
 })
 
-server.listen(443, () => {
-  console.log('Server is running on port 443')
+server.listen(3000, () => {
+  console.log('Server is running on port 3000')
 })
